e440a4b4ec6375c1c8cbc452073300834211aedf
Ly Cao <lycao@10-18-236-232.dynapool.wireless.nyu.edu>
2019-02-17 13:17:45 -0500

    added start script

--- server/server.js
+++ server/server.js
@@ -12,29 +12,10 @@ const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 //var io = socketIO(server);
-var users = new Users();
+//var users = new Users();
 
 app.use(express.static(publicPath));
 
-
-// io.on('connection',(socket)=>{
-//     console.log('New user connected');
-//     socket.on('join', (params, callback)=>{
-       
-//         socket.join(params.room);
-//         users.addUser(params.name, params.pal, params.room);
-
-        
-//         socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-
-//         callback();
-//     });
-
-//     socket.on('disconnect', ()=>{
-//         console.log('User was disconnected');
-//     });
-
-// });
 
 
 server.listen(port, ()=>{
